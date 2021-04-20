f81c94b2d29f1f8ae3d3621ef9b2191149c52512
Angeli Rinawati Silaban <26angelsilaban@gmail.com>
2021-04-20 18:48:23 +0700

    Menambahkan file form.php

---
+++ form.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+ <title>Form</title>
+</head>
+<body>
+<h2>Form Data Diri </h2>
+ <form method = 'post'>
+  <table>
+   <tr>
+    <td>Nama</td>
+    <td>:</td>
+    <td><input type='text' name='nama'></td>
+   </tr>
+   <tr>
+    <td>Umur</td>
+    <td>:</td>
+    <td><input type='text' name='umur'></td>
+   </tr>
+   <tr>
+    <td>Alamat</td>
+    <td>:</td>
+    <td><input type='text' name='alamat'></td>
+   </tr>
+   <tr>
+    <td></td>
+    <td></td>
+    <td><input type='submit' value='kirim'></td>
+   </tr>
+  </table>  
+ </form>
+</body>
+</html>
